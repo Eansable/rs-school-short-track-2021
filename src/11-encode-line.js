d42f1059fd000773ffe8dac42084532569dfa243
Andrei Tarasevich <eansable@yandex.by>
2021-04-10 02:47:06 +0300

    feat: task #11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,32 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+const addText = (tempStr = '') => {
+  let text = '';
+  if (tempStr.length === 1) {
+    text = tempStr;
+  } else {
+    text = `${tempStr.length}${tempStr[0]}`;
+  }
+  return text;
+};
+
+function encodeLine(str) {
+  if (str === '') {
+    return str;
+  }
+  let result = '';
+  let tempStr = str[0];
+  for (let i = 1; i < str.length; i++) {
+    if (tempStr[0] === str[i]) {
+      tempStr += str[i];
+    } else {
+      result += addText(tempStr);
+      tempStr = str[i];
+    }
+  }
+  result += addText(tempStr);
+  return result;
 }
 
 module.exports = encodeLine;
