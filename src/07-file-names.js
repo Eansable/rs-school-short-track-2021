f6b61163ef58e6cba0a7cfbb9737f43b1ebf62cb
Andrei Tarasevich <eansable@yandex.by>
2021-04-10 22:46:28 +0300

    feat: add decide task #7

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,18 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  for (let i = 0; i < names.length; i++) {
+    let index = 1;
+    for (let j = i + 1; j < names.length; j++) {
+      if (names[i] === names[j]) {
+        const tempName = `${names[j]}(${index})`;
+        names.splice(j, 1, tempName);
+        index++;
+      }
+    }
+  }
+  return names;
 }
 
 module.exports = renameFiles;
